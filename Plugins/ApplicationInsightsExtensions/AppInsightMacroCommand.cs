8ceb03784eb8521f0bac288316ac605489c1e0db
DotnetchickUa <DotnetchickUa@ukr.net>
2026-02-24 18:50:21 +0200

    Add ApplicationInsights

---
+++ Plugins/ApplicationInsightsExtensions/AppInsightMacroCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace ApplicationInsightsExtensions;
+
+using System.Net.Http.Headers;
+using System.Web;
+public abstract class AppInsightMacroCommand
+{
+	protected const string AppInsightUrl = "https://api.applicationinsights.io/v1/apps/{0}/query?query={1}";
+
+	protected static HttpClient BuildClient(string? apiKey)
+	{
+		var client = new HttpClient();
+		client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+		client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+		return client;
+	}
+
+	protected static string BuildUrl(string? appId, string? query, string? timespan = null)
+	{
+		var url = string.Format(AppInsightUrl, appId, HttpUtility.UrlEncode(query));
+		if (!string.IsNullOrEmpty(timespan))
+		{
+			url += $"&timespan={timespan}";
+		}
+
+		return url;
+	}
+}
---
+++ Plugins/ApplicationInsightsExtensions/Query.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using Shared;
+
+namespace ApplicationInsightsExtensions;
+
+using System.Text.Json;
+
+[SpeakUpTool]
+public class Query : AppInsightMacroCommand
+{
+	[Description("The query to execute against Application Insights")]
+	public async Task<object?> GetResultsAsync(string appId, string query, string apiKey, CancellationToken cancellationToken)
+	{
+		var url = BuildUrl(appId, query);
+		using var client = BuildClient(apiKey);
+		var response = await client.GetAsync(url, cancellationToken);
+		if (!response.IsSuccessStatusCode)
+		{
+			return null;
+		}
+
+		var json = await response.Content.ReadAsStreamAsync(cancellationToken);
+		return JsonSerializer.DeserializeAsync<object>(json, cancellationToken: cancellationToken);
+	}
+}
