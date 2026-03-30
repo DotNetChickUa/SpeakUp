397857fb59582344502b919aa08d7bce4b05ee7e
DotnetchickUa <dotnetchickua@ukr.net>
2026-03-30 15:52:21 +0300

    Add more converters

--- PluginTester/Program.cs
+++ PluginTester/Program.cs
@@ -2,5 +2,8 @@
 
 //Console.WriteLine(await PhoneValidation.IsPhoneNumberValid("", "", CancellationToken.None));
 await OfficeExtensions.OfficeCommands.WordToPdf("1.docx", "1.pdf");
-OfficeExtensions.OfficeCommands.WordToPdf4("1.docx", "4.pdf");
-OfficeExtensions.OfficeCommands.WordToPdf5("1.docx", "5.pdf");
+await OfficeExtensions.OfficeCommands.WordToPdfWordToPdfConverter("1.docx", "2.pdf");
+await OfficeExtensions.OfficeCommands.WordToPdfNextWordToPdfConverter("1.docx", "3.pdf");
+await OfficeExtensions.OfficeCommands.WordToPdfSpireDoc("1.docx", "4.pdf");
+OfficeExtensions.OfficeCommands.WordToPdfCOM("C:\\Projects\\SpeakUp\\PluginTester\\bin\\Debug\\net10.0\\1.docx", "C:\\Projects\\SpeakUp\\PluginTester\\bin\\Debug\\net10.0\\5.pdf");
+await OfficeExtensions.OfficeCommands.WordToPdfSyncfusion("1.docx", "6.pdf");
--- Plugins/OfficeExtensions/Class1.cs
+++ Plugins/OfficeExtensions/Class1.cs
@@ -1,14 +1,22 @@
 ﻿using BootlegRealists.Reporting;
+using Microsoft.Office.Interop.Word;
 using Shared;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
 using System.ComponentModel;
-using WnvWordToPdf;
+using Winnovative.Pdf.Next;
+using Document = Microsoft.Office.Interop.Word.Document;
+using Task = System.Threading.Tasks.Task;
+using WordToPdfConverter = WnvWordToPdf.WordToPdfConverter;
 
 namespace OfficeExtensions;
 
 [SpeakUpTool]
 public class OfficeCommands
 {
-    [Description("Converts a Word document to PDF")]
+    [Description("Converts a Word document to PDF using DocxToPdf")]
     public static async Task WordToPdf(string docxFileName, string pdfFileName)
     {
         await using var docxStream = new FileStream(docxFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
@@ -18,8 +26,8 @@ public class OfficeCommands
         docxToPdf.Execute(docxStream, pdfStream, runProperties);
     }
 
-    [Description("Converts a Word document to PDF 4")]
-    public static async Task WordToPdf4(string docxFileName, string pdfFileName)
+    [Description("Converts a Word document to PDF using Spire Doc")]
+    public static async Task WordToPdfSpireDoc(string docxFileName, string pdfFileName)
     {
         Spire.Doc.Document document = new Spire.Doc.Document();
         document.LoadFromFile(docxFileName);
@@ -27,8 +35,8 @@ public class OfficeCommands
         document.SaveToFile(pdfFileName, Spire.Doc.FileFormat.PDF);
     }
 
-    [Description("Converts a Word document to PDF 5")]
-    public static async Task WordToPdf5(string docxFileName, string pdfFileName)
+    [Description("Converts a Word document to PDF using WordToPdfConverter")]
+    public static async Task WordToPdfWordToPdfConverter(string docxFileName, string pdfFileName)
     {
         WordToPdfConverter wordToPdfConverter = new WordToPdfConverter();
         wordToPdfConverter.LicenseKey = "DYOSgpaRgpKClIySgpGTjJOQjJubm5uCkg==";
@@ -36,6 +44,36 @@ public class OfficeCommands
         wordToPdfConverter.PdfDocumentOptions.ShowHeader = true;
         wordToPdfConverter.PdfDocumentOptions.ShowFooter = true;
         byte[] outPdfBuffer = wordToPdfConverter.ConvertWordFile(docxFileName);
-        await System.IO.File.WriteAllBytesAsync(pdfFileName, outPdfBuffer);
+        await File.WriteAllBytesAsync(pdfFileName, outPdfBuffer);
+    }
+
+    [Description("Converts a Word document to PDF using Next WordToPdfConverter")]
+    public static async Task WordToPdfNextWordToPdfConverter(string docxFileName, string pdfFileName)
+    {
+        //Licensing.LicenseKey = "DYOSgpaRgpKClIySgpGTjJOQjJubm5uCkg==";
+        var wordToPdfConverter = new Winnovative.Pdf.Next.WordToPdfConverter();
+        await wordToPdfConverter.ConvertToPdfFileAsync(docxFileName, pdfFileName);
+    }
+
+    [Description("Converts a Word document to PDF using COM")]
+    public static void WordToPdfCOM(string docxFileName, string pdfFileName)
+    {
+        Application appWord = new Application();
+        var wordDocument = appWord.Documents.Open(docxFileName);
+        wordDocument.ExportAsFixedFormat(pdfFileName, WdExportFormat.wdExportFormatPDF);
+        wordDocument.Close();
+        appWord.Quit();
+    }
+
+    [Description("Converts a Word document to PDF using Syncfusion")]
+    public static async Task WordToPdfSyncfusion(string docxFileName, string pdfFileName, string? licenseKey = null)
+    {
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(licenseKey);
+        await using FileStream docStream = new FileStream(docxFileName, FileMode.Open, FileAccess.Read);
+        using WordDocument wordDocument = new WordDocument(docStream, FormatType.Docx);
+        using DocIORenderer render = new DocIORenderer();
+        using var pdfDocument = render.ConvertToPDF(wordDocument);
+        await using FileStream stream = new FileStream(pdfFileName, FileMode.Create, FileAccess.Write);
+        pdfDocument.Save(stream);
     }
 }
